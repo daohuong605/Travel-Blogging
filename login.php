7992c7a9b7d11ec249be1882a6024fc6618b50c8
kimngoc03 <147018374+kimngoc03@users.noreply.github.com>
2024-05-21 11:57:36 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,131 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-    <!-- Design by foolishdeveloper.com -->
-    <title>Login Page</title>
-=======
   <!-- Design by foolishdeveloper.com -->
     <title>Login Page</title>
  
->>>>>>> 3bec55014980cf0183ef64d7e89ab3265714786f
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
     <!--Stylesheet-->
     <style media="screen">
-<<<<<<< HEAD
-        *,
-        *::before,
-        *::after {
-            padding: 0;
-            margin: 0;
-            box-sizing: border-box;
-        }
-        body {
-            background: linear-gradient(to right, #6a11cb, #2575fc);
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-family: 'Poppins', sans-serif;
-        }
-        form {
-            height: 520px;
-            width: 400px;
-            background-color: rgba(255, 255, 255, 0.13);
-            position: absolute;
-            transform: translate(-50%, -50%);
-            top: 50%;
-            left: 50%;
-            border-radius: 10px;
-            backdrop-filter: blur(10px);
-            border: 2px solid rgba(255, 255, 255, 0.1);
-            box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
-            padding: 50px 35px;
-        }
-        form * {
-            color: #ffffff;
-            letter-spacing: 0.5px;
-            outline: none;
-            border: none;
-        }
-        form h3 {
-            font-size: 32px;
-            font-weight: 500;
-            line-height: 42px;
-            text-align: center;
-        }
-        label {
-            display: block;
-            margin-top: 30px;
-            font-size: 16px;
-            font-weight: 500;
-        }
-        input {
-            display: block;
-            height: 50px;
-            width: 100%;
-            background-color: rgba(255, 255, 255, 0.07);
-            border-radius: 3px;
-            padding: 0 10px;
-            margin-top: 8px;
-            font-size: 14px;
-            font-weight: 300;
-        }
-        ::placeholder {
-            color: #e5e5e5;
-        }
-        button {
-            margin-top: 50px;
-            width: 100%;
-            background-color: #ffffff;
-            color: #080710;
-            padding: 15px 0;
-            font-size: 18px;
-            font-weight: 600;
-            border-radius: 5px;
-            cursor: pointer;
-            transition: background-color 0.3s;
-        }
-        button:hover {
-            background-color: #eaeaea;
-        }
-        .social {
-            margin-top: 30px;
-            display: flex;
-            justify-content: space-between;
-        }
-        .social div {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width: 150px;
-            height: 40px;
-            border-radius: 3px;
-            background-color: rgba(255, 255, 255, 0.27);
-            color: #eaf0fb;
-            cursor: pointer;
-            transition: background-color 0.3s;
-        }
-        .social div:hover {
-            background-color: rgba(255, 255, 255, 0.47);
-        }
-        .social i {
-            margin-right: 10px;
-        }
-        a {
-            display: block;
-            text-align: center;
-            margin-top: 20px;
-            color: #eaf0fb;
-            text-decoration: none;
-            transition: color 0.3s;
-        }
-        a:hover {
-            color: #ff512f;
-        }
-=======
       *,
 *:before,
 *:after{
@@ -134,7 +17,7 @@
     box-sizing: border-box;
 }
 body{
-    background-color: #080710;
+    background-color: #080710 ;
 }
 
 
@@ -219,35 +102,36 @@ button{
 .social i{
   margin-right: 4px;
 }
+.link {
+  margin-top: 20px;
+}
+a {
+  display: block;
+  text-align: center;
+  margin-top: 20px;
+  color: #eaf0fb;
+  text-decoration: none;
+  transition: color 0.3s;
+}
+a:hover {
+  color: #ff512f;
+}
 
->>>>>>> 3bec55014980cf0183ef64d7e89ab3265714786f
     </style>
 </head>
 <body>
-    <?php include('header.php'); ?>
+    
     <form method="post">
         <h3>Login Here</h3>
         <label for="username">Username</label>
-<<<<<<< HEAD
-        <input type="text" placeholder="Enter Username" id="username" name="username" required>
-
-        <label for="password">Password</label>
-        <input type="password" placeholder="Password" id="password" name="password" required>
-
-        <button type="submit">Log In</button>
-        <a href="signup.php">Create an account</a>
-    </form>
-    <?php include('footer.php'); ?>
-=======
         <input type="text" placeholder="Enter Username" id="username" name="username">
 
         <label for="password">Password</label>
         <input type="password" placeholder="Password" id="password" name="pass">
 
         <button type="submit">Log In</button>
-        <li class="link"><a href="signup.php">Create a account</a></li>
+        <div class="link"><a href="signup.php">Create a new account</a></div>
     </form>
-    <?php include('footer.php');?>
->>>>>>> 3bec55014980cf0183ef64d7e89ab3265714786f
+    
 </body>
 </html>
